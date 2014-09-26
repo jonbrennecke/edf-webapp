3d5a53c998710701a1db55615920bbbb03740ba9
Jon Brennecke <jpbrennecke@gmail.com>
2014-09-26 13:25:26 -0700

    install scripts needs to rename nodewebkits package.json file

--- install.js
+++ install.js
@@ -4,9 +4,19 @@
 // install script //////////////////////////////////////////
 ////////////////////////////////////////////////////////////
 
-var spawn = require('child_process').spawn;
+// because of a bug in Nodewebkit 0.8.6
+// we need to rename node_modules/nodewebkit/package.json to 
+// node_modules/nodewebkit/_package.json
+var fs = require('fs');
 
-// process.chdir( __dirname+'/public/node_modules/edflib/' );
+fs.rename(__dirname+'/node_modules/nodewebkit/package.json', __dirname+'/node_modules/nodewebkit/_package.json', function(err) {
+    if ( err ) console.log('ERROR: ' + err);
+});
+
+
+// install edflib
+
+var spawn = require('child_process').spawn;
 
 // install edflib
 process.chdir( __dirname+'/public/' );
