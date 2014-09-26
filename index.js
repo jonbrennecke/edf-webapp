b48dd6d1de703283d574f966f7807c4613f2ffba
jonbrennecke <brennecke.jonathan@wsu.edu>
2014-09-26 14:06:15 -0700

    making the start script work in windows

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 		// findpath = require('nodewebkit').findpath;  // TODO only works in NW >=10.0
 		// nwpath = findpath(),
 		nwpath = './node_modules/nodewebkit/bin/nodewebkit',
-		nw = spawn(nwpath,[ __dirname + '/public/', '--no-toolbar' ]);
+		nw = spawn('node',[ nwpath, __dirname + '/public/' ]);
 
 	// nw.stdout.pipe(process.stdout); // TODO disable in production
 	// nw.stderr.pipe(process.stderr); // TODO disable in production
